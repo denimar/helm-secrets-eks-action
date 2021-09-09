97b6e9655b813e152de0bb21abe8fe3d39e61e06
Denimar <denimar@shopolive.com>
2021-09-09 20:37:24 -0300

    initial commit

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,17 +2,17 @@
 
 set -e
 
-version="$1"
+kubectlVersion="$1"
 config="$2"
 command="$3"
 
-if [ "$version" = "latest" ]; then
-  version=$(curl -Ls https://dl.k8s.io/release/stable.txt)
+if [ "$kubectlVersion" = "latest" ]; then
+  kubectlVersion=$(curl -Ls https://dl.k8s.io/release/stable.txt)
 fi
 
-echo "using kubectl@$version"
+echo "using kubectl@$kubectlVersion"
 
-curl -sLO "https://dl.k8s.io/release/$version/bin/linux/amd64/kubectl" -o kubectl
+curl -sLO "https://dl.k8s.io/release/$kubectlVersion/bin/linux/amd64/kubectl" -o kubectl
 chmod +x kubectl
 mv kubectl /usr/local/bin
 
