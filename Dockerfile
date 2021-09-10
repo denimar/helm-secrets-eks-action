4d1bed8e9f396b38844740072be79a3a0c537e96
Denimar <denimar@shopolive.com>
2021-09-09 23:03:01 -0300

    installed curl

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM alpine:latest
+FROM golang:alpine3.14
 
 LABEL version="1.0.0"
 LABEL name="helm-secrets-eks-action"
--- entrypoint.sh
+++ entrypoint.sh
@@ -24,18 +24,6 @@ mv kubectl /usr/local/bin
 # Installing helm secrets
 helm plugin install "https://github.com/jkroepke/helm-secrets" --version v3.8.3
 
-# Installing go
-apk add --no-cache --virtual .build-deps bash gcc musl-dev openssl go 
-wget -O go.tgz https://dl.google.com/go/go1.10.3.src.tar.gz 
-tar -C /usr/local -xzf go.tgz 
-cd /usr/local/go/src/ 
-./make.bash 
-export PATH="/usr/local/go/bin:$PATH"
-export GOPATH=/opt/go/ 
-export PATH=$PATH:$GOPATH/bin 
-apk del .build-deps 
-go version
-
 # Installing sops
 go get -u go.mozilla.org/sops/v3/cmd/sops
 cd $GOPATH/src/go.mozilla.org/sops/
