39d833057829b4c47ed3e56589e63db8569c5401
Denimar <denimar@shopolive.com>
2021-09-09 22:40:37 -0300

    installed curl

--- entrypoint.sh
+++ entrypoint.sh
@@ -25,8 +25,8 @@ mv kubectl /usr/local/bin
 helm plugin install "https://github.com/jkroepke/helm-secrets" --version v3.8.3
 
 # Installing go
-apk add .build-deps gcc go
-wget -O go.tgz https://dl.google.com/go/go1.10.3.src.tar.gz 
+apk add .build-deps gcc go 
+wget -O go.tgz https://golang.org/dl/go1.10.3.linux-amd64.tar.gz
 tar -C /usr/local -xzf go.tgz 
 cd /usr/local/go/src/ 
 ./make.bash 
@@ -34,6 +34,7 @@ export PATH="/usr/local/go/bin:$PATH"
 export GOPATH=/opt/go/ 
 export PATH=$PATH:$GOPATH/bin 
 apk del .build-deps 
+go version
 
 # Installing sops
 go get -u go.mozilla.org/sops/v3/cmd/sops
